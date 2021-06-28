ef84132c5738b47128f361e2c122622f844212a7
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2021-06-29 00:42:47 +0200

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -11,43 +11,17 @@ import {
   Service
 } from "homebridge";
 
-/*
- * IMPORTANT NOTICE
- *
- * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the "hap-nodejs" module).
- * The above import block may seem like, that we do exactly that, but actually those imports are only used for types and interfaces
- * and will disappear once the code is compiled to Javascript.
- * In fact you can check that by running `npm run build` and opening the compiled Javascript file in the `dist` folder.
- * You will notice that the file does not contain a `... = require("homebridge");` statement anywhere in the code.
- *
- * The contents of the above import statement MUST ONLY be used for type annotation or accessing things like CONST ENUMS,
- * which is a special case as they get replaced by the actual value and do not remain as a reference in the compiled code.
- * Meaning normal enums are bad, const enums can be used.
- *
- * You MUST NOT import anything else which remains as a reference in the code, as this will result in
- * a `... = require("homebridge");` to be compiled into the final Javascript code.
- * This typically leads to unexpected behavior at runtime, as in many cases it won't be able to find the module
- * or will import another instance of homebridge causing collisions.
- *
- * To mitigate this the {@link API | Homebridge API} exposes the whole suite of HAP-NodeJS inside the `hap` property
- * of the api object, which can be acquired for example in the initializer function. This reference can be stored
- * like this for example and used to access all exported variables and classes from HAP-NodeJS.
- */
 let hap: HAP;
 
-/*
- * Initializer function called when the plugin is loaded.
- */
 export = (api: API) => {
   hap = api.hap;
-  api.registerAccessory("ExampleSwitch", ExampleSwitch);
+  api.registerAccessory("Huawei B535", HuaweiB535);
 };
 
-class ExampleSwitch implements AccessoryPlugin {
+class HuaweiB535 implements AccessoryPlugin {
 
   private readonly log: Logging;
   private readonly name: string;
-  private switchOn = false;
 
   private readonly switchService: Service;
   private readonly informationService: Service;
@@ -57,36 +31,39 @@ class ExampleSwitch implements AccessoryPlugin {
     this.name = config.name;
 
     this.switchService = new hap.Service.Switch(this.name);
+    
     this.switchService.getCharacteristic(hap.Characteristic.On)
       .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
-        log.info("Current state of the switch was returned: " + (this.switchOn? "ON": "OFF"));
-        callback(undefined, this.switchOn);
+        log.info("State of the router queried");
+        
+        // Router is always On - otherwise you wouldn't be able to connect to the bridge.
+        callback(undefined, true); 
       })
-      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        this.switchOn = value as boolean;
-        log.info("Switch state was set to: " + (this.switchOn? "ON": "OFF"));
-        callback();
-      });
+      .on(CharacteristicEventTypes.SET, this.setSwitchState.bind(this));
 
     this.informationService = new hap.Service.AccessoryInformation()
-      .setCharacteristic(hap.Characteristic.Manufacturer, "Custom Manufacturer")
-      .setCharacteristic(hap.Characteristic.Model, "Custom Model");
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Huawei")
+      .setCharacteristic(hap.Characteristic.Model, "B535")
+      .setCharacteristic(hap.Characteristic.SerialNumber, "???");
 
-    log.info("Switch finished initializing!");
+    log.info("Router finished initializing!");
   }
 
-  /*
-   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
-   * Typical this only ever happens at the pairing process.
-   */
+  setSwitchState(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    const switchOn = value as boolean;
+    this.log.info("Router was turned " + (switchOn ? "ON": "OFF"));
+
+    if (!switchOn) {
+      // TODO: Restart the router
+    }
+    
+    callback();
+  }
+
   identify(): void {
     this.log("Identify!");
   }
 
-  /*
-   * This method is called directly after creation of this instance.
-   * It should return all services which should be added to the accessory.
-   */
   getServices(): Service[] {
     return [
       this.informationService,
