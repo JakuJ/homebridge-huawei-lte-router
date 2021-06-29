4041546548a53cfe4c0464e375dce216117c3956
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2021-06-29 14:13:25 +0200

    Async router information getters

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,20 @@ import {
   AccessoryConfig,
   AccessoryPlugin,
   API,
-  CharacteristicEventTypes,
-  CharacteristicGetCallback,
   CharacteristicSetCallback,
   CharacteristicValue,
   HAP,
   Logging,
   Service
 } from "homebridge";
+
+import { Device } from "huawei-lte-api";
 import { connect } from "./router";
+import {Mutex, MutexInterface, Semaphore, SemaphoreInterface, withTimeout} from 'async-mutex';
 
 let hap: HAP;
 
-export = (api: API) => {
+export = async (api: API) => {
   hap = api.hap;
   api.registerAccessory("Huawei LTE Router", Router);
 };
@@ -24,32 +25,46 @@ class Router implements AccessoryPlugin {
   private readonly log: Logging;
   private readonly name: string;
   private readonly config: AccessoryConfig;
-
+  
+  private device: Device | null = null;
+  private info: Record<string, unknown> | null = null;
+  private readonly mutex: Mutex;
+  
   private readonly switchService: Service;
-  private readonly informationService: Service;
+  private informationService: Service;
 
   constructor(log: Logging, config: AccessoryConfig, api: API) {
     this.log = log;
     this.config = config;
     this.name = config.name;
+    this.mutex = new Mutex();
 
     this.switchService = new hap.Service.Switch(this.name);
-    
-    this.switchService.getCharacteristic(hap.Characteristic.On)
-      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
-        log.info("State of the router queried");
-        
-        // Router is always On - otherwise you wouldn't be able to connect to the bridge.
-        callback(undefined, true); 
-      })
-      .on(CharacteristicEventTypes.SET, this.setSwitchState.bind(this));
-
     this.informationService = new hap.Service.AccessoryInformation()
-      .setCharacteristic(hap.Characteristic.Manufacturer, "Huawei")
-      .setCharacteristic(hap.Characteristic.Model, "Dummy model") // TODO
-      .setCharacteristic(hap.Characteristic.SerialNumber, "Dummy number"); // TODO
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Huawei");
 
-    log.info("Router finished initializing!");
+    this.informationService.getCharacteristic(hap.Characteristic.Model).on(hap.CharacteristicEventTypes.GET, async (callback) => {
+      
+      const value = await this.mutex.runExclusive(async () => await this.getInfo('DeviceName'));
+      callback(undefined, value);
+    });
+
+    this.informationService.getCharacteristic(hap.Characteristic.SerialNumber).on(hap.CharacteristicEventTypes.GET, async (callback) => {
+      const value = await this.mutex.runExclusive(async () => await this.getInfo('SerialNumber'));
+      callback(undefined, value);
+    });
+
+    this.informationService.getCharacteristic(hap.Characteristic.SoftwareRevision).on(hap.CharacteristicEventTypes.GET, async (callback) => {
+      const value = await this.mutex.runExclusive(async () => await this.getInfo('SoftwareVersion'));
+      callback(undefined, value);
+    });
+
+    this.switchService.getCharacteristic(hap.Characteristic.On)
+    .on(hap.CharacteristicEventTypes.GET, callback => {
+      this.log.info("State of the router queried");
+      callback(undefined, this.device !== null);
+    })
+    .on(hap.CharacteristicEventTypes.SET, this.setSwitchState.bind(this));
   }
 
   setSwitchState(value: CharacteristicValue, callback: CharacteristicSetCallback) {
@@ -58,14 +73,33 @@ class Router implements AccessoryPlugin {
 
     if (!switchOn) {
       // TODO: Restart the router
-      connect(this.config.password);
     }
     
     callback();
   }
 
+  async getDevice() {
+    if (this.device === null) {
+      this.log.debug("Connecting to the device");
+      this.device = await connect(this.config.password);
+      this.log.debug("Connected");
+    }
+    
+    return this.device;
+  }
+  
+  async getInfo(key: string) {
+    if (this.info === null) {
+      this.log.debug("Fetching information");
+      this.info = await (await this.getDevice()).information();
+      this.log.debug("Information:", this.info);
+    }
+    
+    return this.info[key] as string;
+  }
+
   identify(): void {
-    this.log("Identify!");
+    this.log("Identify");
   }
 
   getServices(): Service[] {
--- src/router.ts
+++ src/router.ts
@@ -1,18 +1,7 @@
 import {Connection, Device } from 'huawei-lte-api';
 
-export function connect(password: string) {
-    const connection = new Connection(`http://admin:${password}@192.168.8.1/`, 5000);
-
-    connection.ready.then(() => {
-        console.log('Connected to router API');
-
-        const device = new Device(connection);
-        device.signal().then(result => {
-            console.log(result);
-        }).catch(error => {
-            console.log(error);
-        });
-
-    });
-
+export async function connect(password: string) {
+    const connection = new Connection(`http://admin:${password}@192.168.8.1`, 5000);
+    await connection.ready;
+    return new Device(connection);
 }
